7db246bf7b494316556d62e1280ef31974d55cee
EmptyGodBady <virtusprodam12@gmail.com>
2024-08-24 10:12:31 +0200

    init

---
+++ src/components/NewDebt/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function NewDebt() {
+  return (
+    <div className="flex flex-col border h-[100px] w-[250px]">
+      <div className="flex justify-evenly border">
+        <div>+</div>
+        <div>123</div>
+        <div>micha</div>
+        <div>vitalii</div>
+      </div>
+      <div className="w-full p-2">comment</div>
+    </div>
+  );
+}
